made sure that the force density vectors computed from PDDO linearisation are now correct! Thez match with the force density vectors obtained from the bond constant calculation

main/PDLinearStatic.py
@@ -134,16 +134,10 @@ class PDFatigueSolver:
         stiffMat = np.zeros((self.FID.coordVec.shape[0]*2,self.FID.coordVec.shape[0]*2),dtype=float)
         return pddo._generate_stiffness_matrix2(self.FID.coordVec,self.FID.neighbors, self.FID.start_idx, self.FID.end_idx, self.FID.G11vec, self.FID.G12vec, self.FID.G22vec, self.FID.muArr, LiveBonds, bondDamage, stiffMat)
     
-    def gen_bond_stiffness_matrices(self) -> np.ndarray:
-        return pddo._generate_bond_stiffnesses(self.FID.coordVec,self.FID.neighbors, self.FID.start_idx, self.FID.end_idx, self.FID.G11vec, self.FID.G12vec, self.FID.G22vec, self.FID.muArr)
-
     def apply_displacement_BC(self,BCvec,stiffnessMat,RHSvec):
         """Only works for dense matrix form"""
         return pddo.applyDispBC2(BCvec,stiffnessMat,RHSvec,dim=self.FID.dim)
-
-    def gen_bond_displacement_vecs(self,dispVec: np.ndarray[float,2]) -> np.ndarray:
-        return pddo._generate_bond_displacement_vecs(dispVec,self.FID.neighbors, self.FID.start_idx, self.FID.end_idx)
-
+    
     def calc_bond_stretches(self,cur_coordVec):
         """Calculate the stretches of each bond"""
         return (pddo.calc_bondLenghts(cur_coordVec,self.FID.neighbors,self.FID.start_idx,self.FID.end_idx)-self.FID.init_BondLens)/self.FID.init_BondLens
@@ -155,8 +149,8 @@ class PDFatigueSolver:
         stiffMat = self.gen_stiffness_matrix(self.FID.curLiveBonds,self.FID.curBondDamage)
         forceDensVec = stiffMat @ disps
         return forceDensVec
-    
-    def solve_for_eq3(self):
+
+    def solve_for_eq3(self) -> np.ndarray:
         """Solves for equlibirum state of the system with desegnated "epsilon" as the maximum residual fraction"""
         BCvec = self.FID.combined_BC_vec
         num_max_it = self.FID.num_max_it

main/disretization.py
@@ -33,14 +33,15 @@ class Discretization():
 
     def __init__(self) -> None:
         self.hasInitialCrack = False
-        self.initialCracks = np.empty(shape=(0,2,2))
+        self.initialCracks = np.zeros(shape=(0,2,2))
 
     def set_horizon(self, horizonRad) -> None:
         self.delta = float(horizonRad)
 
     def create_crack(self,p1x,p1y,p2x,p2y) -> None:
         crack = np.array([[[p1x,p1y],[p2x,p2y]]],dtype=float)
-        self.initialCracks.append(crack)
+        self.initialCracks = np.append(self.initialCracks,crack,axis=0)
+        # self.initialCracks.append(crack)
         self.hasInitialCrack = True
 
     def deactivate_cracks(self) -> bool:
@@ -83,11 +84,11 @@ class Discretization():
             i += 1
             
         if self.hasInitialCrack == True:
-            self.neighbors, self.start_idx, self.end_idx, self.n_neighbors = pddo.find_neighbors2(self.coordVec,1.01*self.delta,self.cracks)
+            self.neighbors, self.start_idx, self.end_idx, self.n_neighbors = pddo.find_neighbors2(self.coordVec,1.01*self.delta,self.initialCracks)
 
         else:
             self.neighbors, self.start_idx, self.end_idx, self.n_neighbors = pddo.find_neighbors(self.coordVec,1.01*self.delta)
-        print("the area:",self.ptArea)
+        # print("the area:",self.ptArea)
 
         self.pd_point_count = self.n_neighbors.shape[0]
         self.pd_bond_count = self.neighbors.shape[0]